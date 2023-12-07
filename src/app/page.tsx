8c1950e131bcc7f06a34d70a2c992a846be7b6c2
yuito-it <yuito_it@outlook.jp>
2023-12-07 18:18:42 +0900

    BackGround added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,51 +6,19 @@ const BASE_PATH = basePath ? basePath : "";
 export default function Home() {
   return (
     <MotionWrapper>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {/*<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>*/}
-
-        {/*<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>*/}
-
-        <div className="flex mb-3 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <main className="flex min-h-screen text-white flex-col items-center justify-between p-24 bg-[url('/BackGround.jpg')]">
+        <div className="flex mb-3 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-left">
           <h1 className={`mb-3 text-4xl font-semibold`}>Untitled Project!</h1>
+        </div>
+
+        <div className="flex mb-3 grid  lg:max-w-5xl lg:w-full lg:mb-40 lg:grid-cols-1 lg:text-left">
+          <p className="mb-3 text-2xl font-semibold">The Digital Club.</p>
         </div>
 
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
             href="/about"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-black hover:bg-black"
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -64,12 +32,11 @@ export default function Home() {
             </p>
           </a>
 
-          <s>
-            <a
-              href="javascript:void(0)"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              rel="noopener noreferrer"
-            >
+          <a
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-black hover:bg-black"
+            rel="noopener noreferrer"
+          >
+            <s>
               <h2 className={`mb-3 text-2xl font-semibold`}>
                 Blog{" "}
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -79,16 +46,15 @@ export default function Home() {
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                 The Blog of member.(Now prepairing...)
               </p>
-            </a>
-          </s>
+            </s>
+          </a>
 
-          <s>
-            <a
-              href="/projects"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+          <a
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-black hover:bg-black"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <s>
               <h2 className={`mb-3 text-2xl font-semibold`}>
                 Projects{" "}
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -98,10 +64,18 @@ export default function Home() {
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                 Explore our projects.
               </p>
-            </a>
-          </s>
+            </s>
+          </a>
         </div>
       </main>
+      <footer className="bg-black">
+        <div>
+          <span className="text-white">
+            (c)Untitled Project 2023, Other Credit are
+            <a href="./credit"> Here</a>
+          </span>
+        </div>
+      </footer>
     </MotionWrapper>
   );
 }
