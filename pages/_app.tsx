4feb727a2297eb49c0f3cf8404ae6dd58c60d930
yuito-it <yuito_it@outlook.jp>
2023-12-29 18:35:07 +0900

    css miss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import usePageView from '../hooks/usePageView'
 import GoogleAnalytics from '../components/GoogleAnalytics'
 
+import '../styles/index.css'
+
 const App = ({ Component, pageProps }) => {
   usePageView() // 追加
 
