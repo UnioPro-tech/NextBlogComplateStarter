37709666cc68c55e0f9702e9933ace6f8086100f
唯人(Yuito Akatsuki) <yuito_it@outlook.jp>
2024-01-23 18:24:54 +0900

    Sitemap-rss (#68)
    * :sparkles: sitemap.xml 生成スクリプトの作成
    
    * :sparkles: sitemap追加
    
    * :zap: 仕組みの変更
    
    * :arrow_up: 依存関係の追加
    この先のプログラムで使うため
    
    * :sparkles: sitemapのジェネレーターを作成
    Google Search Consoleに登録のため
    
    * :sparkles: sitemapスクリプトをpackage.jsonに追加
    
    * :sparkles: ワークフローにsitemapを自動生成するように追記

---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,49 @@
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+import { create } from "xmlbuilder";
+
+const postsDirectory = join(process.cwd(), "_posts");
+
+const getPosts = () => {
+  const fileNames = readdirSync(postsDirectory);
+  const posts = fileNames.map((fileName) => {
+    const fullPath = join(postsDirectory, fileName);
+    const fileContents = readFileSync(fullPath, "utf8");
+    const { data } = matter(fileContents);
+    return {
+      ...data,
+      slug: fileName.replace(/\.md$/, ""),
+    };
+  });
+  return posts;
+};
+
+const generateSitemapXml = (posts) => {
+  const root = create("urlset", { version: "1.0", encoding: "UTF-8" });
+  root.attribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+  posts.forEach((post) => {
+    const url = root.ele("url");
+    url.ele("loc", `https://unipro-tech.github.io/posts/${post.slug}`);
+    url.ele("lastmod", post.date);
+    url.ele("changefreq", "monthly");
+    url.ele("priority", "0.8");
+  });
+
+  let url = root.ele("url");
+  url.ele("loc", `https://unipro-tech.github.io`);
+  url.ele("lastmod", posts[posts.length - 1].date);
+  url.ele("priority", "0.8");
+
+  return root.end({ pretty: true });
+};
+
+const main = () => {
+  const posts = getPosts();
+  const sitemapXml = generateSitemapXml(posts);
+  writeFileSync("public/maps.xml", sitemapXml, "utf8");
+  console.log("Sitemap generated successfully!");
+};
+
+main();
